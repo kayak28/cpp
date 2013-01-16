51e202496f5a6af7aa79b873b880b03e41e6f935
28kayak <kaya_ota@msn.com>
2013-01-16 14:27:57 -0800

    initial commit

---
+++ NamedConstant.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const double RATE = 6.9;
+	double deposit; 
+	
+	cout << "Enter the amount of your deposit $";
+	cin >> deposit;
+
+	double newBalance;
+	newBalance = deposit + deposit * (RATE/100);
+	cout << "In one year, that deposit will grow to\n";
+	cout << "$" << newBalance << " an amount worth waiting for.\n";
+	
+	return 0;
+
+}
+
+
